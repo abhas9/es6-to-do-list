8456a01725309b5d7a5fdcc7db652024dd00fc3b
Abhas Tandon <atandon@paypal.com>
2015-09-12 02:52:01 -0700

    Initial commit with gulp and babel

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var babel = require("gulp-babel");
+
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var babelify = require("babelify");
+
+gulp.task('clean', function () {
+    return gulp.src('build/*', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('movestatic', function(){
+    gulp.src(['src/**/*.{html,css,js}',
+                    'src/*.{html,css,js}'])
+   .pipe(gulp.dest('./build'));
+});
+
+gulp.task('babelify', function(){
+    browserify('./src/js/app.js')
+        .transform(babelify)
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./build/js/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['src/**/*.{html,css,js}',
+                'src/*.{html,css,js}'], ['movestatic']);
+});
+
+gulp.task('default', ['movestatic', 'babelify']);
