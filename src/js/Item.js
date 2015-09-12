681d291dd77d8448e67c1b9a12ecb05ec1989de7
Abhas Tandon <abhastandon007@gmail.com>
2015-09-12 04:10:46 -0700

    Add view for Item

--- src/js/Item.js
+++ src/js/Item.js
@@ -8,6 +8,12 @@ var Item = class {
     this.date = date;
     this.status = status;
   }
+  render() {
+    return `<li data-id="${this.id}" data-status="${this.status}">
+              ${this.title}
+              <span>${this.date}</span>
+            </li>`
+  }
 }
 
 module.exports = Item;
